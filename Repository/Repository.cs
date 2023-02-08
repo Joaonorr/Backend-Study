b33068b7e816532b8838558efc67e880671eade1
João Norões Menezes Castro <0@lead.fortaleza>
2023-02-08 02:12:04 -0300

    feat: adding managerial interface and concrete repository

---
+++ Repository/Repository.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using WebApplication1.Context;
+
+namespace WebApplication1.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        protected AppDbContext _context;
+
+        public Repository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            return _context.Set<T>().AsNoTracking();
+        }
+
+        public T Get(Expression<Func<T, bool>> predicate)
+        {
+            return _context.Set<T>().SingleOrDefault(predicate);
+        }
+
+        public void Add(T entity)
+        {
+            _context.Set<T>().Add(entity);
+        }
+
+        public void Update(T entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+            _context.Set<T>().Update(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+        }
+    }
+}
