b41178b00ae2103673439ab48b69fdc999b51c1d
Joaonorr <joaonoroes.jn@gmail.com>
2023-02-13 01:14:29 -0300

    feat: create method for organizing services

--- Program.cs
+++ Program.cs
@@ -4,30 +4,12 @@ using System.Text.Json.Serialization;
 using WebApplication1.Context;
 using WebApplication1.DTOs.Mappings;
 using WebApplication1.Repository;
+using WebApplication1.services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers().AddJsonOptions(options => 
-options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var MappingConfig = new MapperConfiguration(mc =>
-{
-    mc.AddProfile(new MappingProfile());
-});
-IMapper mapper = MappingConfig.CreateMapper();
-builder.Services.AddSingleton(mapper);
-
-// Configure DataBase Connection
-var mySqlConnection = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<AppDbContext>(options => 
-    options.UseMySql(mySqlConnection, ServerVersion.AutoDetect(mySqlConnection)));
-
-builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+// configure method
+builder.ConfigureServices();
 
 var app = builder.Build();
 
---
+++ services/Services.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
+using WebApplication1.Context;
+using WebApplication1.DTOs.Mappings;
+using WebApplication1.Repository;
+
+namespace WebApplication1.services;
+
+public static class Services
+{
+    public static void ConfigureServices(this WebApplicationBuilder builder)
+    {
+        // Add services to the container.
+
+        builder.Services.AddControllers().AddJsonOptions(options =>
+        options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var MappingConfig = new MapperConfiguration(mc =>
+        {
+            mc.AddProfile(new MappingProfile());
+        });
+        IMapper mapper = MappingConfig.CreateMapper();
+        builder.Services.AddSingleton(mapper);
+
+        // Configure DataBase Connection
+        var mySqlConnection = builder.Configuration.GetConnectionString("DefaultConnection");
+        builder.Services.AddDbContext<AppDbContext>(options =>
+            options.UseMySql(mySqlConnection, ServerVersion.AutoDetect(mySqlConnection)));
+
+        builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+    }
+}
